1a26444aaf386c95fefc82d1bdcbcf943c96f2bc
kovaIenko <ruskov004@gmail.com>
2019-02-12 11:19:09 +0200

    upload files

---
+++ memoseeds/Models/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using memoseeds.Models.Entities;
+
+namespace memoseeds.Database
+{
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+            }
+
+       // public DbSet<User> Users { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Category> Categories { get; set; }
+     
+    }
+    }
+
---
+++ memoseeds/Models/Entities/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace memoseeds.Models.Entities
+{
+    public class Subject
+    {
+        public Subject()
+        { }
+        public int subjectId { get; set;}
+        public String name { get; set; }
+        public virtual ICollection<Category> categories { get; set; }
+
+    }
+}
