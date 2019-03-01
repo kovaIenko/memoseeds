8e1b4f0e9e25cb14c6110980c43937a373169edd
Vladyslav <vladokthrr@gmail.com>
2019-03-01 19:05:33 +0200

    update trans interface

--- memoseeds/Services/ITranslatorService.cs
+++ memoseeds/Services/ITranslatorService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 namespace memoseeds.Services
 {
     public interface ITranslatorService
     {
-       // List<string> getTranslatesByTerm(LanguageCodes from, LanguageCodes to, string term);
+        string translate (string sourceText, string sourceLanguage, string targetLanguage);
     }
 }
