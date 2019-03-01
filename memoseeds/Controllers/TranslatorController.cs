f15ad8ac1ec038d38695fd5c56f98e30fe3b1d04
Vladyslav <vladokthrr@gmail.com>
2019-03-01 19:11:04 +0200

    Update trans controller
    Now simple translation for all languages is offered. User sends to server json post reqeust, controller parses it. Having three main values, especially text, targeredLang, sourceLang, web server creates http request and sends to the translator api some data. Finally, server returns asking translation to User.

--- memoseeds/Controllers/TranslatorController.cs
+++ memoseeds/Controllers/TranslatorController.cs
@@ -1,5 +1,12 @@
-﻿using System;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using memoseeds.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace memoseeds.Controllers
 {
@@ -7,5 +14,32 @@ namespace memoseeds.Controllers
     {
         // TODO реалізувати API для фронтенду.  
         // Інформація передаватиметься за допомогою json
+
+            //All languages format should be Full. I.E. English, Ukrainian, Bulgarian 
+        public void translateMethod(String json){
+            //getting json object
+            JObject jObject = JObject.Parse(json);
+            //parsing it
+            string sourceText = (string)jObject.SelectToken("sourceText");
+            string sourceLanguage = (string)jObject.SelectToken("sourceLanguage");
+            string targetLanguage = (string)jObject.SelectToken("targetLanguage");
+
+            //creates object that manages translation
+            TranslatorService ts = new TranslatorService();
+            //translates user text from A language to B language
+            String transResponse = ts.translate(sourceText, sourceLanguage, targetLanguage);
+
+            //sending response
+            string response_json = "{\"translation\":\"" + transResponse + "\"}";
+            Response.Clear();
+            Response.ContentType = "application/json; charset=utf-8";
+            Response.Write(json);
+            Response.End();
+
+        } 
+
+
     }
+
 }
+
