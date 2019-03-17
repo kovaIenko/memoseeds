3501216493016f1787ba5e60b22eb27f3e2932e9
kovaIenko <ruskov004@gmail.com>
2019-03-17 22:53:04 +0200

    configure Entity User

--- memoseeds/Models/Entities/User.cs
+++ memoseeds/Models/Entities/User.cs
@@ -1,14 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace memoseeds.Models.Entities
 {
     public class User
     {
-        public int userId { get; set; }
-        public int courseId { get; set; }
 
-        public bool isLocal { get; set; }
+        public User(string name, string email, string password)
+        {
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "id")]
+        public long userId { get; set; }
+
+        public string name { get; set; }
+
+        public string password{ get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal money { get; set; }
+
+        public string email { get; set; }
 
         public virtual ICollection<Collector> collectors { get; set; }
     }
