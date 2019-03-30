804db956f8eadfaf27b8686f699197909dd77580
naumchyk <andriy.nau@gmail.com>
2019-03-30 19:52:55 +0200

    Add pepos and contr
    Add user repositiry and study controller

--- memoseeds/Controllers/StudyController.cs
+++ memoseeds/Controllers/StudyController.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using memoseeds.Models.Entities;
+using memoseeds.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace memoseeds.Controllers
 {
+
+    [ApiController]
     public class StudyController:  Controller
     {
+        private ModuleRepository moduleRepository;
+        private UserRepository userRepository;
+
+            public StudyController(ModuleRepository ModuleRepository, UserRepository userRepository)
+        {
+            this.moduleRepository = ModuleRepository;
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet("api/getUser")]
+        public JsonResult getUs()
+        {
+            return Json(userRepository.getUserByName("kovalenko"));
+        }
+
+        [HttpPost("api/getModule")]
+        public JsonResult getModule([FromBody]long id)
+        {
+            Module m = moduleRepository.GetById(id);
+            return Json(m);
+        }
+
+        [HttpPost("api/updateUser")]
+        public void updateUser([FromBody] UserInfo user, ModuleInfo module)
+        {
+
+        }
+
+
+
+
         // TODO реалізувати API для фронтенду, узгодити із Сокуром,
         // який розроблятиме відповідний сервіс на фронті. 
         // Інформація передаватиметься за допомогою json
     }
+
+    public class ModuleInfo
+    {
+
+
+
+    }
+
+    public class UserInfo
+    {
+        internal object country;
+
+        public string Username { get; set; }
+    }
+
+
 }
---
+++ memoseeds/Repositories/UserRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using memoseeds.Database;
+using memoseeds.Models.Entities;
+
+namespace memoseeds.Repositories
+{
+    public class UserRepository : IRepository<User>, IDisposable
+    {
+
+
+        public User getUserByName(string name)
+        {
+            return context.Users.Find(name);
+        }
+
+        private bool disposed = false;
+        private ApplicationDbContext context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(long Id)
+        {
+           context.Users.Remove(GetById(Id));
+        }
+
+        public User GetById(long id)
+        {
+            return context.Users.Find(id);
+        }
+
+        public void Insert(User entity)
+        {
+            context.Users.Add(entity);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(User entity)
+        {
+            context.Users.Update(entity);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+
+
+}
+}
